0a31fa99213db2c074421a2f8f149b30d2e307f7
Abdulrahmanfawzy <abdulrahman2d77@gmail.com>
2025-02-27 17:43:45 +0200

    update hook

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,19 @@ import NotFound from "./components/NotFound/NotFound";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import CartRoute from "./components/CartRoute/CartRoute";
+import useWindowSize from "./components/customHook/windowHook";
 
 export default function App() {
-  
+  const { width, height } = useWindowSize();
+
   return (
     <BrowserRouter>
       <Provider store={store}>
         <Navbar />
-
+        <div style={{position: "fixed" , bottom: "20px", left: "20px"}}>
+          <p>Width: {width}px</p>
+          <p>Height: {height}px</p>
+        </div>
         <Routes>
           <Route index path="/" element={<Home/>}/>
           <Route path="/cart" element={<CartRoute/>}/>
